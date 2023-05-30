3c4be67e5863b2d902c9e2ff0473828563686e28
peartedio <PAV120100@gmail.com>
2023-05-30 18:30:01 +0300

    Path algorithm

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-def dfs(graph, start, visited=None):
+def dfs(graph, start, visited=None, to=0, path_len=0):
+    if start == to:
+        return visited, path_len, True
+
     if visited is None:
         visited = set()
+
     visited.add(start)
 
-    print(start)
-
     for next in graph[start] - visited:
-        dfs(graph, next, visited)
-    return visited
+        path_len += 1
+        _, _, is_find = dfs(graph, next, visited, to=to, path_len=path_len)
+        if is_find:
+            return visited.union({next}), path_len + 1, True
 
-
-graph = {'0': set(['1', '2']),
-         '1': set(['0', '3', '4']),
-         '2': set(['0']),
-         '3': set(['1']),
-         '4': set(['2', '3'])}
-
-dfs(graph, '0')
+    return visited, path_len, False
